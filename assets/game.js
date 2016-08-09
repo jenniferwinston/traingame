2636e71561f19d8faa6b3f0d34e7cfdb3d65fe9b
Jennifer Winston <jenniferwinston@jennifestonsmbp.attlocal.net>
2016-08-09 16:17:28 -0400

    working on minutes

--- assets/game.js
+++ assets/game.js
@@ -27,8 +27,31 @@
   		})
   		$("input").val('');
   		return false;
-  	}) // onclick
+  	
 
+
+ 	var tFrequency = 3;
+		var firstTime = time; 
+		// First Time (pushed back 1 year to make sure it comes before current time)
+		var firstTimeConverted = moment(firstTime,"hh:mm").subtract(1, "years");
+		console.log(firstTimeConverted);
+		// Current Time
+		var currentTime = moment();
+		console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+		// Difference between the times
+		var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+		console.log("DIFFERENCE IN TIME: " + diffTime);
+		// Time apart (remainder)
+		var tRemainder = diffTime % tFrequency;
+		console.log(tRemainder);
+		// Minute Until Train
+		var tMinutesTillTrain = tFrequency - tRemainder;
+		console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+		// Next Train
+		var nextTrain = moment().add(tMinutesTillTrain, "minutes")
+		console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))	
+
+}); // onclick
 
   database.ref().on("child_added", function(childSnapshot){
 	// pull the data
@@ -37,17 +60,20 @@
 	console.log("Destination: " + train.destination);
 	console.log("Time: " + train.time);
 	console.log("Frequency: " + train.frequency);
-	console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+	console.log(moment().format('LT'));
 	moment("YYYYMMDD").fromNow();
 	
 	//display in the top table
 	var newElement = $("<tr/>").attr("data-name", train.name);
 	  newElement.append($("<td/> ").text(train.name));
 	  newElement.append($("<td/> ").text(train.destination));
-	  newElement.append($("<td/> ").text(train.time));
 	  newElement.append($("<td/> ").text(train.frequency));
+	  newElement.append($("<td/> ").text(train.nextTrain)); // need to change to a time
+	  newElement.append($("<td/> ").text(train.tMinutesTillTrain));
 	  $(".table").append(newElement);
 
 });	// child added
+
+
 
 	
